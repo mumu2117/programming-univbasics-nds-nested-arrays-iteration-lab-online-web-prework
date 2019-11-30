8618753ba5a61cbf07a98b67f066be1e9320c487
algebraic-designer-6218 <jmiller@archersystems.us>
2019-11-30 23:04:53 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -6,12 +6,16 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
+   foods=[ ["cheese", "no cheese"], ["grilled chicken", "pepperoni"]
+  puts ('I love my pizza with' + array[0] + "and"+ array[1])
 end
 
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
+  src = [ :group1[1, 5] :group2[34,11]]
+  srcg = [group1.max, group2.max]
 end
 
 def total_even_pairs(src)
@@ -22,4 +26,17 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+  src = [ :n1[5, 4], :n2[6, 9], :n3[1,1] ]
+  if  n1%1==0 && n1.to_i.even?
+    total1 = n1[0]+n1[1]
+    puts total1
+    elsif 
+    n%1==0 && n2.to_i.even?
+    total2 = n2[0]+n2[1]
+    puts total2
+    elsif 
+    n%1==0 && n3.to_i.even?
+    total3 = n3[0]+n1[1]
+    puts total3
+  else NIL
 end
